c8543ea5d752a58ea30934d4a237c0bc32a35afd
Kyle Hinton <kylehinton3@gmail.com>
2025-12-04 23:59:39 +0000

    AoC-2025 - Optimizations added for future reference, kept old code but commented out so I can peek bad solutions

--- Day4/CSharp/Paper.cs
+++ Day4/CSharp/Paper.cs
@@ -30,6 +30,14 @@ public class PaperGrid
   // Dictionary to store all grid locations and their value
   public Dictionary<(int x, int y), (char itemToFind, bool accessible)> gridLocations = new Dictionary<(int x, int y), (char itemToFind, bool accessible)>();
 
+  // Optimization - Predefining offsets rather than using GetAdjacentLocations method to create a new list each time
+  public (int x, int y)[] offsetCoords = 
+  {
+    (0, -1), (1, -1), (-1, -1),
+    (0, 1),  (1, 1),  (-1, 1),
+    (1, 0),  (-1, 0)
+  };
+
   public void PopulateGrid(int xCoord, int yCoord, char value)
   {
     // Adds location to the dictionary, obviously :D 
@@ -47,23 +55,24 @@ public class PaperGrid
     gridLocations[(xCoord, yCoord)] = ('.', false);
   }
 
-  public List<(int x, int y)> GetAdjacentLocations(int xCoord, int yCoord)
-  {
-    // Check all adjacent items N, NE, NW, S, SE, SW, E, W
-    var adjacentLocations = new List<(int x, int y)>
-    {
-      (xCoord, yCoord - 1), // North
-      (xCoord + 1, yCoord - 1), // Northeast
-      (xCoord - 1, yCoord - 1), // Northwest
-      (xCoord, yCoord + 1), // South
-      (xCoord + 1, yCoord + 1), // Southeast
-      (xCoord - 1, yCoord + 1), // Southwest
-      (xCoord + 1, yCoord), // East
-      (xCoord - 1, yCoord), // West
-    };
+  // Optimization - Didn't want to remove it so I can remember the unoptimized version to avoid in future
+  // public List<(int x, int y)> GetAdjacentLocations(int xCoord, int yCoord)
+  // {
+  //   // Check all adjacent items N, NE, NW, S, SE, SW, E, W
+  //   var adjacentLocations = new List<(int x, int y)>
+  //   {
+  //     (xCoord, yCoord - 1), // North
+  //     (xCoord + 1, yCoord - 1), // Northeast
+  //     (xCoord - 1, yCoord - 1), // Northwest
+  //     (xCoord, yCoord + 1), // South
+  //     (xCoord + 1, yCoord + 1), // Southeast
+  //     (xCoord - 1, yCoord + 1), // Southwest
+  //     (xCoord + 1, yCoord), // East
+  //     (xCoord - 1, yCoord), // West
+  //   };
 
-    return adjacentLocations;
-  }
+  //   return adjacentLocations;
+  // }
 
   public bool ValidLocation(int xCoord, int yCoord)
   {
@@ -79,11 +88,72 @@ public class PaperGrid
     return false;
   }
 
-  public int AccessibleRolls()
+  public int AdjacentCount(int xCoord, int yCoord, char itemToFind)
   {
-    // Initialize item to find
-    char itemToFind = '@';
-    var totalAccessibleItems = 0;
+    var adjacentCount = 0;
+
+    foreach (var offset in offsetCoords)
+    {
+      int adjacentX = xCoord + offset.x;
+      int adjacentY = yCoord + offset.y;
+
+      if (ValidLocation(adjacentX, adjacentY) && RollExists(adjacentX, adjacentY, itemToFind))
+      {
+        adjacentCount++;
+      }
+    }
+
+    return adjacentCount;
+  }
+
+// Optimization - Entire method rewritten to use AdjacentCount rather than GetAdjacentLocations
+//   public int AccessibleRolls()
+//   {
+//     // Initialize item to find
+//     char itemToFind = '@';
+//     var totalAccessibleItems = 0;
+
+//     foreach (var key in gridLocations.Keys)
+//     {
+//       // Skip over items that we don't care about
+//       if (RollExists(key.x, key.y, itemToFind) == false)
+//       {
+//         continue;
+//       }
+
+//       var adjacentLocations = GetAdjacentLocations(key.x, key.y);
+//       var adjacentPaperRolls = 0;
+//       foreach (var location in adjacentLocations)
+//       {
+//         // If the key exists in the dictionary and the value matches the item to find, increase the adjacenet paper roll count
+//         if (ValidLocation(location.x, location.y) && RollExists(location.x, location.y, itemToFind))
+//         {
+//           adjacentPaperRolls++;
+//         }
+//       }
+
+//       // If there are less than 4 adjacent paper rolls, mark this as accessible, default is to mark as false so this SHOULD allow us to only count the accessible ones
+//       if (adjacentPaperRolls < 4)
+//       {
+//         UpdateAccessible(key.x, key.y, true);
+//       }
+//     }
+
+//     // Loop through all grid locations again and count the accessible items
+//     foreach (var key in gridLocations.Keys)
+//     {
+//       if (RollExists(key.x, key.y, itemToFind) && gridLocations[key].accessible)
+//       {
+//         totalAccessibleItems++;
+//       }
+//     }
+//     return totalAccessibleItems;
+//   }
+
+  public Queue<(int x, int y)> GetAccessibleRolls(char itemToFind)
+  {
+    // Initialize queue to store accessible rolls, which will allow us to process only the accessible items for removal instead of having to loop through the entire grid each time
+    var accessibleRolls = new Queue<(int x, int y)>();
 
     foreach (var key in gridLocations.Keys)
     {
@@ -93,33 +163,16 @@ public class PaperGrid
         continue;
       }
 
-      var adjacentLocations = GetAdjacentLocations(key.x, key.y);
-      var adjacentPaperRolls = 0;
-      foreach (var location in adjacentLocations)
-      {
-        // If the key exists in the dictionary and the value matches the item to find, increase the adjacenet paper roll count
-        if (ValidLocation(location.x, location.y) && RollExists(location.x, location.y, itemToFind))
-        {
-          adjacentPaperRolls++;
-        }
-      }
-
-      // If there are less than 4 adjacent paper rolls, mark this as accessible, default is to mark as false so this SHOULD allow us to only count the accessible ones
+      // Store count of adjacent paper rolls, rather than getting the list of adjacent locations then counting them, we can do it in one go
+      var adjacentPaperRolls = AdjacentCount(key.x, key.y, itemToFind);
       if (adjacentPaperRolls < 4)
       {
-        UpdateAccessible(key.x, key.y, true);
+        // Add to accessible rolls queue
+        accessibleRolls.Enqueue((key.x, key.y));
       }
     }
 
-    // Loop through all grid locations again and count the accessible items
-    foreach (var key in gridLocations.Keys)
-    {
-      if (RollExists(key.x, key.y, itemToFind) && gridLocations[key].accessible)
-      {
-        totalAccessibleItems++;
-      }
-    }
-    return totalAccessibleItems;
+    return accessibleRolls;
   }
 }
 
@@ -164,3 +217,8 @@ public class PaperGrid
 // Using a while loop against the total accessible to remove accessible items until none are left, constantly updating the accessibility after each removal
 
 // Part two solved but it's SLOW as hell
+
+// Optimizations:
+// Predefined offsets rather than creating a new list each time in GetAdjacentLocations - Comment added above also to show this, remember this for future use, it may come up again
+// Rewrote AccessibleRolls to use AdjacentCount rather than GetAdjacentLocations to avoid creating lists and looping through them multiple times
+// 
--- Day4/CSharp/Program.cs
+++ Day4/CSharp/Program.cs
@@ -14,7 +14,6 @@ var paperGrid = new PaperGrid();
 // Initialize item to find
 char itemToFind = '@';
 var totalAccessibleItems = 0;
-var totalRemovedRolls = 0;
 
 // Create stopwatch for performance measurement
 Stopwatch stopwatch = new Stopwatch();
@@ -29,35 +28,87 @@ foreach (var input in inputs)
   }
 }
 
-
-// Part 1 - Determine accessible items
+// Part 1 Optimized
 stopwatch.Start();
-
-totalAccessibleItems = paperGrid.AccessibleRolls();
-
+totalAccessibleItems = paperGrid.GetAccessibleRolls(itemToFind).Count;
 stopwatch.Stop();
 
 Console.WriteLine($"Total accessible '{itemToFind}' items: {totalAccessibleItems}");
 Console.WriteLine($"Time taken for Part 1: {stopwatch.ElapsedMilliseconds} ms");
 
-// Part 2 - Remove accessible items until none are left, updating accessibility as we g
+
+// Part 2 Optimized
+stopwatch.Reset();
 stopwatch.Start();
+var accessibleRolls = paperGrid.GetAccessibleRolls(itemToFind);
+var processedRolls = new HashSet<(int x, int y)>();
+var totalRemovedRolls = 0;
 
-while (totalAccessibleItems > 0)
+while (accessibleRolls.Count > 0)
 {
-  foreach (var key in paperGrid.gridLocations.Keys)
-  {
-    if (paperGrid.RollExists(key.x, key.y, itemToFind) && paperGrid.gridLocations[key].accessible)
-    {
-      paperGrid.RemoveRoll(key.x, key.y);
-      totalRemovedRolls++;
+  var currentRoll = accessibleRolls.Dequeue();
 
-      // After removing a roll, we need to re-evaluate accessibility for all rolls
-      totalAccessibleItems = paperGrid.AccessibleRolls();
+  if (!paperGrid.RollExists(currentRoll.x, currentRoll.y, itemToFind))
+  {
+    continue;
+  }
+
+  paperGrid.RemoveRoll(currentRoll.x, currentRoll.y);
+  totalRemovedRolls++;
+
+  int[] xOffset = { 0, 1, -1, 0, 1, -1, 1, -1 };
+  int[] yOffset = { -1, -1, -1, 1, 1, 1, 0, 0 };
+
+  for (int i = 0; i < xOffset.Length; i++)
+  {
+    int adjacentX = currentRoll.x + xOffset[i];
+    int adjacentY = currentRoll.y + yOffset[i];
+
+    if (paperGrid.gridLocations.ContainsKey((adjacentX, adjacentY)) && paperGrid.RollExists(adjacentX, adjacentY, itemToFind))
+    {
+      int adjacentCount = paperGrid.AdjacentCount(adjacentX, adjacentY, itemToFind);
+      
+      if (adjacentCount < 4)
+      {
+        accessibleRolls.Enqueue((adjacentX, adjacentY));
+      }
     }
   }
 }
-
 stopwatch.Stop();
 Console.WriteLine($"Total removed '{itemToFind}' items: {totalRemovedRolls}");
 Console.WriteLine($"Time taken for Part 2: {stopwatch.ElapsedMilliseconds} ms");
+
+
+// Commented out for optimization
+// // Part 1 - Determine accessible items
+// stopwatch.Start();
+
+// totalAccessibleItems = paperGrid.AccessibleRolls();
+
+// stopwatch.Stop();
+
+// Console.WriteLine($"Total accessible '{itemToFind}' items: {totalAccessibleItems}");
+// Console.WriteLine($"Time taken for Part 1: {stopwatch.ElapsedMilliseconds} ms");
+
+// // Part 2 - Remove accessible items until none are left, updating accessibility as we g
+// stopwatch.Start();
+
+// while (totalAccessibleItems > 0)
+// {
+//   foreach (var key in paperGrid.gridLocations.Keys)
+//   {
+//     if (paperGrid.RollExists(key.x, key.y, itemToFind) && paperGrid.gridLocations[key].accessible)
+//     {
+//       paperGrid.RemoveRoll(key.x, key.y);
+//       totalRemovedRolls++;
+
+//       // After removing a roll, we need to re-evaluate accessibility for all rolls
+//       totalAccessibleItems = paperGrid.AccessibleRolls();
+//     }
+//   }
+// }
+
+// stopwatch.Stop();
+// Console.WriteLine($"Total removed '{itemToFind}' items: {totalRemovedRolls}");
+// Console.WriteLine($"Time taken for Part 2: {stopwatch.ElapsedMilliseconds} ms");
