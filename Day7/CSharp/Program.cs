d4aab334293c1e0a82eefd0f797c29d847752405
Kyle Hinton <kylehinton3@gmail.com>
2025-12-07 23:18:36 +0000

    AoC-2025 - Day 7 Part 2

--- Day7/CSharp/Program.cs
+++ Day7/CSharp/Program.cs
@@ -1,13 +1,25 @@
-﻿// using System.Diagnostics;
+﻿using System.Diagnostics;
 string[] inputLines = File.ReadAllLines("Day7\\input.txt"); // Import inputs
+var stopwatch = new Stopwatch();
 
+stopwatch.Start();
 var teleporter = new Day7.Teleporter(inputLines);
 int startPos = teleporter.GetStartPos();
 Console.WriteLine($"Start position: {startPos}");
 teleporter.GetMovements(startPos);
 teleporter.Move();
-
+stopwatch.Stop();
+Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
 Console.WriteLine($"Total splits: {teleporter.TotalSplits}");
+
+stopwatch.Reset();
+stopwatch.Start();
+teleporter = new Day7.Teleporter(inputLines);
+teleporter.GetTimelines();
+stopwatch.Stop();
+Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"Total timelines: {teleporter.TotalTimelines}");
+
 // S = start
 // Always move down
 // . = open space
--- Day7/CSharp/Teleporter.cs
+++ Day7/CSharp/Teleporter.cs
@@ -5,6 +5,7 @@ public class Teleporter
   private string[] _input;
   private Queue<int[]> _movements = new Queue<int[]>();
   private int _totalSplits = 0;
+  private long _totalTimelines = 0;
   private int _row = 0;
   private char _startChar = 'S';
   private char _beamChar = '|';
@@ -12,6 +13,7 @@ public class Teleporter
   private char _openSpaceChar = '.';
 
   public int TotalSplits => _totalSplits;
+  public long TotalTimelines => _totalTimelines;
 
   public Teleporter(string[] input)
   {
@@ -54,7 +56,7 @@ public class Teleporter
       }
 
       NextRow();
-      PrintGrid(); // Just used for debugging
+      // PrintGrid(); // Just used for debugging
     }
   }
 
@@ -87,6 +89,55 @@ public class Teleporter
       }
 
       _movements = movementList;
+  }
+
+  public void GetTimelines()
+  {
+    _row = 0;
+
+    var timelines = new Dictionary<int, long>(); // Position / Count of timelines
+    timelines[GetStartPos()] = 1; // Start with one timeline at the start position
+
+    while (_row < _input.Length)
+    {
+      var newTimeline = new Dictionary<int, long>();
+
+      foreach (var timeline in timelines) // Go through each position in the current timeline
+      {
+        int position = timeline.Key; // Position in the current timeline
+        long count = timeline.Value; // Number of timelines reaching this position, AddToTimeline will handle adding this count to new timelines
+
+        if (position < 0 || position >= _input[_row].Length)
+        {
+          continue; // Out of bounds
+        }
+
+        char currentChar = _input[_row][position];
+      
+        if (currentChar == _splitterChar)
+        {
+          AddToTimeline(newTimeline, position - 1, count); // Split the timeline
+          AddToTimeline(newTimeline, position + 1, count); // Split the timeline
+        }
+        else
+        {
+          AddToTimeline(newTimeline, position, count); // Continue down the same position
+        }
+      }
+      timelines = newTimeline; // Update the timelines to the new timeline for the next row
+      NextRow(); // Move to the next row
+    }
+
+    _totalTimelines = timelines.Values.Sum(); // +1 for the initial timeline
+  }
+
+  public void AddToTimeline(Dictionary<int, long> timelines, int position, long count)
+  {
+      if (!timelines.ContainsKey(position))
+      {
+          timelines[position] = 0; // If the row isn't already present in the timeline, initialize it to 0
+      }
+      timelines[position] += count; // Otherwise add the count var (number of timelines reaching this position) to the existing count
   }
 
   public void PrintGrid()
